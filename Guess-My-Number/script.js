29d27f03b814ff1c2f509e130a676de59e97ab1c
Yash Bansal <Yash.Bansal@wissen.com>
2024-04-30 12:39:18 +0530

    Refactor the code DRY principle Finish the Project

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -5,12 +5,13 @@
  //random number to be generated
 let number = Math.round(Math.random()*100)
 console.log(number)
-
 //initial score
 let score = 20
-
 let highscore = 0
 
+const displayMessage = (message) => {
+    document.querySelector('.message').textContent = message;
+}
 //when check button is clicked
 document.querySelector('.check').addEventListener('click', function(){
 
@@ -19,11 +20,11 @@ const guess = Number(document.querySelector('.guess').value)
 
 //when there is no input
 if(!guess){
-    document.querySelector('.message').textContent = '⛔️ No number!'
+    displayMessage('⛔️ No number!')
 }
 //when player wins
 else if(guess === number){
-    document.querySelector('.message').textContent = `🎉 Correct Number !`
+    displayMessage(`🎉 Correct Number !`)
     let currentScore = document.querySelector('.score').textContent
     if(currentScore > highscore) {
     highscore = currentScore
@@ -35,34 +36,47 @@ else if(guess === number){
 
 }
 
-//when score is too low
-else if (guess < number) {
+//when score is too low or too high
+else if (guess !== number) {
     if(score<=0){
-        document.querySelector('.message').textContent = 'You Lost the Game !'
+        displayMessage('You Lost the Game !')
+        document.querySelector('.score').textContent = 0
     }
     else {
-    document.querySelector('.message').textContent = 'Too low !'
+    document.querySelector('.message').textContent = guess>number?'Too high !':'Too low !'
     document.querySelector('.score').textContent = `${--score}`
     }
 }
 
-// when score is too high
- else if (guess > number) {
-    if(score<=0){
-        document.querySelector('.message').textContent = 'You Lost the Game !'
-    }
-    else {
-    document.querySelector('.message').textContent = 'Too high !'
-    document.querySelector('.score').textContent = `${--score}`
-    }
-}
+// else if (guess < number) {
+//     if(score<=0){
+//         document.querySelector('.message').textContent = 'You Lost the Game !'
+//         document.querySelector('.score').textContent = 0
+//     }
+//     else {
+//     document.querySelector('.message').textContent = 'Too low !'
+//     document.querySelector('.score').textContent = `${--score}`
+//     }
+// }
+
+// // when score is too high
+//  else if (guess > number) {
+//     if(score<=0){
+//         document.querySelector('.message').textContent = 'You Lost the Game !'
+//         document.querySelector('.score').textContent = 0
+//     }
+//     else {
+//     document.querySelector('.message').textContent = 'Too high !'
+//     document.querySelector('.score').textContent = `${--score}`
+//     }
+// }
 })
 
 //when again button is clicked
 document.querySelector('.again').addEventListener('click', ()=>{
     score = 20
     number = Math.round(Math.random()*100)
-    document.querySelector('.message').textContent = 'Start Guessing ...'
+    displayMessage('Start Guessing ...')
     document.querySelector('.number').textContent = '?'
     document.querySelector('body').style.backgroundColor = '#222'
     document.querySelector('.number').style.width = '15rem'
