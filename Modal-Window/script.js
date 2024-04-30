1fd659e2a9029260dec081de00dc01c0fa72719e
Yash Bansal <Yash.Bansal@wissen.com>
2024-04-30 12:57:04 +0530

    Add modal window functionality to all buttons

--- Modal-Window/script.js
+++ Modal-Window/script.js
@@ -1,11 +1,13 @@
 'use strict';
 
-document.querySelector('.show-modal').addEventListener(
-    'click', () => {
-       document.querySelector('.modal').classList.remove('hidden');
-       document.querySelector('.overlay').classList.remove('hidden');
-    }
-)
+document.querySelectorAll('.show-modal').forEach((button)=>{
+    button.addEventListener(
+        'click', () => {
+           document.querySelector('.modal').classList.remove('hidden');
+           document.querySelector('.overlay').classList.remove('hidden');
+        }
+    )
+})
 
 document.querySelector('.close-modal').addEventListener('click', ()=> {
     document.querySelector('.modal').classList.add('hidden');
