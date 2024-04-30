6b9167b4e29ac0d052baae7801832a89f57027e1
Yash Bansal <Yash.Bansal@wissen.com>
2024-04-30 13:18:15 +0530

    Refactor DRY Principle

--- Modal-Window/script.js
+++ Modal-Window/script.js
@@ -1,15 +1,30 @@
 'use strict';
 
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+const closeModal = ()=> {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+const openModal = () => {
+    modal.classList.remove('hidden');
+    // or modal.style.display = 'block';
+    overlay.classList.remove('hidden');
+ }
+
+
 document.querySelectorAll('.show-modal').forEach((button)=>{
     button.addEventListener(
-        'click', () => {
-           document.querySelector('.modal').classList.remove('hidden');
-           document.querySelector('.overlay').classList.remove('hidden');
-        }
+        'click', openModal
     )
+
+    // button.addEventListener(, () => {
+    //     modal.classList.remove('hidden');
+    //     overlay.classList.remove('hidden');
+    //  })
 })
 
-document.querySelector('.close-modal').addEventListener('click', ()=> {
-    document.querySelector('.modal').classList.add('hidden');
-    document.querySelector('.overlay').classList.add('hidden');
-})
+document.querySelector('.close-modal').addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
