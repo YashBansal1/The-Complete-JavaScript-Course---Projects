299df5e908c39e5f4fa5446bb2736495a9d72ea6
Yash Bansal <Yash.Bansal@wissen.com>
2024-04-30 12:54:38 +0530

    Add the functionality to show modal 1

--- Modal-Window/script.js
+++ Modal-Window/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+document.querySelector('.show-modal').addEventListener(
+    'click', () => {
+       document.querySelector('.modal').classList.remove('hidden');
+       document.querySelector('.overlay').classList.remove('hidden');
+    }
+)
+
+document.querySelector('.close-modal').addEventListener('click', ()=> {
+    document.querySelector('.modal').classList.add('hidden');
+    document.querySelector('.overlay').classList.add('hidden');
+})
