cff7fa93c67de54e60452188584b1a5e275e8b52
Yash Bansal <Yash.Bansal@wissen.com>
2024-05-09 11:33:21 +0530

    Add dates for our app for withdrawal and deposits

--- Bankist_App/script.js
+++ Bankist_App/script.js
@@ -10,6 +10,19 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
@@ -17,6 +30,20 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account3 = {
@@ -24,6 +51,20 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
+
 };
 
 const account4 = {
@@ -31,6 +72,21 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+
+  currency: 'USD',
+  locale: 'en-US',
+
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -61,17 +117,9 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-// const currencies = new Map([
-//   ['USD', 'United States dollar'],
-//   ['EUR', 'Euro'],
-//   ['GBP', 'Pound sterling'],
-// ]);
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
-//containerApp.style.opacity = 100 ;
 const displayMovements = function (acc, sort=false) {
+  labelDate.textContent = new Date().toLocaleDateString();
   containerMovements.innerHTML = '';
 
   const  movs = sort ? acc.movements.slice().sort((a, b) => a-b): acc.movements;
@@ -82,8 +130,8 @@ const displayMovements = function (acc, sort=false) {
 
     const html = ` <div class="movements__row">
     <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
-    <div class="movements__date">3 days ago</div>
-    <div class="movements__value">${mov} €</div>
+    <div class="movements__date">${new Date(acc.movementsDates[i]).toLocaleDateString()}</div>
+    <div class="movements__value">${mov.toFixed(2)} €</div>
   </div>`;
   
   containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -93,18 +141,18 @@ const displayMovements = function (acc, sort=false) {
 const calDisplayBalance = function(acc) {
   const balance = acc.movements.reduce((acc, mov) => acc+mov, 0);
   acc.balance = balance;
-  labelBalance.textContent = `${balance} €`
+  labelBalance.textContent = `${acc.balance.toFixed(2)} €`
 
 }
 
 const calDisplaySummary = function(account) {
   const income = account.movements.filter(mov=>mov>0).reduce((acc, mov)=>acc+mov, 0);
-  labelSumIn.textContent = `${income} €`;
+  labelSumIn.textContent = `${income.toFixed(2)} €`;
   const out = account.movements.filter(mov=>mov<0).reduce((acc, mov)=>acc+mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)} €`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)} €`;
 
   const interest = account.movements.filter(mov=>mov>0).map(deposit => deposit*account.interestRate/100).filter(int => int>=1).reduce((acc, int)=>acc+int, 0);
-  labelSumInterest.textContent = `${interest} €`;
+  labelSumInterest.textContent = `${interest.toFixed(2)} €`;
 
 }
 const createUserName = function(accs) {
@@ -148,6 +196,8 @@ btnLogin.addEventListener('click', (e)=>{
    }
   else
    console.log("Not logged in");
+
+  
   });
 
   btnTransfer.addEventListener('click',  (e)=>{
@@ -159,6 +209,8 @@ btnLogin.addEventListener('click', (e)=>{
     {
       currentAccount.movements.push(-amount);
       receiverAcc.movements.push(amount);
+      currentAccount.movementsDates.push(new Date());
+      receiverAcc.movementsDates.push(new Date());
       updateUI();
     }
     else
@@ -168,10 +220,11 @@ btnLogin.addEventListener('click', (e)=>{
 
   btnLoan.addEventListener('click',  (e)=>{
     e.preventDefault();
-    const amount = Number(inputLoanAmount.value);
+    const amount = Math.floor(inputLoanAmount.value);
     if(amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1))
     {
       currentAccount.movements.push(amount);
+      currentAccount.movementsDates.push(new Date());
       updateUI();
     }
     else
@@ -182,7 +235,7 @@ btnLogin.addEventListener('click', (e)=>{
   btnClose.addEventListener('click', (e)=>{
       e.preventDefault();
       const userName = inputCloseUsername.value;
-      const pin = Number(inputClosePin.value);
+      const pin = +(inputClosePin.value);
       if(currentAccount.userName ==  userName && currentAccount.pin == pin)
         {
           const index = accounts.findIndex(acc => acc.userName = currentAccount.userName);
@@ -194,9 +247,12 @@ btnLogin.addEventListener('click', (e)=>{
         else
         console.log("Can't Delete the account");
   })
+
   let sorted = false;
   btnSort.addEventListener('click', (e)=>{
     e.preventDefault();
     sorted = !sorted;
     displayMovements(currentAccount, sorted);
   })
+
+  
